f7ac392a91cbfccd442e15fd2304938209ee6e5c
Mauricio Crecencio <mauriciocrecencio98@gmail.com>
2021-04-26 22:53:54 -0300

    add constants file

---
+++ utils/constants.js
@@ -0,0 +1,5 @@
+// If modifying these scopes, delete token.json.
+export const scopes = ["https://www.googleapis.com/auth/spreadsheets"];
+export const keyFile = "credentials.json";
+export const spreadsheetId = "1F1z3g8zm5R4Eh1lvPoTlcd33EI000CY9rOSbzZnunao";
+export const sheetName = "engenharia_de_software";
